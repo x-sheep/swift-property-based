4255eb8b21f0627f1f4ee6ad22a84627da67df39
Lennard Sprong <lennard@buku.io>
2025-05-19 13:09:29 +0200

    Re-add all Float generators

--- Sources/PropertyBased/Gen+Float.swift
+++ Sources/PropertyBased/Gen+Float.swift
@@ -26,3 +26,73 @@ extension Gen where Value: BinaryFloatingPoint & Sendable, Value.RawSignificand:
         )
     }
 }
+
+extension Gen where Value == Double {
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func double(in range: ClosedRange<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+    
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func double(in range: Range<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+}
+
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
+extension Gen where Value == Float16 {
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func float16(in range: ClosedRange<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+    
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func float16(in range: Range<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+}
+
+#if !(os(Windows) || os(Android)) && (arch(i386) || arch(x86_64))
+extension Gen where Value == Float80 {
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func float80(in range: ClosedRange<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+    
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func float80(in range: Range<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+}
+#endif
+
+#if canImport(Foundation)
+import Foundation
+extension Gen where Value == CGFloat {
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func cgFloat(in range: ClosedRange<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+    
+    /// Returns a generator of random values within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value. `range` must be finite.
+    /// - Returns: A generator of random values within the bounds of range.
+    @inlinable
+    public static func cgFloat(in range: Range<Value>) -> Generator<Value, Shrink.FloatingPointShrinkSequence<Value>> { return float(in: range) }
+}
+#endif
