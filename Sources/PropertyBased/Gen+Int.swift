4fd24738e7e266b7de46ad88fb92fe0da31981a4
Lennard Sprong <lennard@buku.io>
2025-05-19 13:06:19 +0200

    Re-add all Integer generators

--- Sources/PropertyBased/Gen+Int.swift
+++ Sources/PropertyBased/Gen+Int.swift
@@ -2,24 +2,107 @@
 // Copyright (c) 2019 Point-Free, Inc. MIT License
 
 extension Gen where Value: FixedWidthInteger & Sendable {
-    /// Returns a generator of random values within the specified range.
+    /// Returns a generator of random integers within the specified range.
     ///
-    /// - Parameter range: The range in which to create a random value. `range` must be finite.
-    /// - Returns: A generator of random values within the bounds of range.
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
     @inlinable
-    public static func int(in range: some RangeExpression<Value>) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> {
-        let closedRange = ClosedRange(range)
+    public static func int(in range: ClosedRange<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> {
         return .init(
-            run: { rng in Value.random(in: closedRange, using: &rng) },
-            shrink: { $0.shrink(within: closedRange) }
+            run: { rng in Value.random(in: range, using: &rng) },
+            shrink: { $0.shrink(within: range) }
         )
     }
     
-    /// Returns a generator of random values within the full range of the integer type.
+    /// Returns a generator of random integers within the specified range.
     ///
-    /// - Returns: A generator of random values.
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
     @inlinable
-    public static func int() -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> {
-        return int(in: Value.min ... Value.max)
+    public static func int(in range: some RangeExpression<Value>) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> {
+        return int(in: ClosedRange(range))
     }
+}
+
+extension Gen where Value == Int8 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func int8(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == Int16 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func int16(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == Int32 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func int32(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == Int64 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func int64(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+
+extension Gen where Value == UInt {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func uint(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == UInt8 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func uint8(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == UInt16 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func uint16(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == UInt32 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func uint32(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+extension Gen where Value == UInt64 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func uint64(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+
+@available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+extension Gen where Value == Int128 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func int128(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
+}
+
+@available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+extension Gen where Value == UInt128 {
+    /// Returns a generator of random integers within the specified range.
+    ///
+    /// - Parameter range: The range in which to create a random value.
+    /// - Returns: A generator of random integers within the bounds of range.
+    @inlinable public static func uint128(in range: some RangeExpression<Value> = Value.min ... Value.max) -> Generator<Value, Shrink.IntegralShrinkSequence<Value>> { int(in: range) }
 }
