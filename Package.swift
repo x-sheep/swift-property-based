af486378d212ecddfa84ceaf1c1592623121141f
Lennard Sprong <lennard@buku.io>
2025-06-03 14:26:43 +0200

    WIP of Property macro

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.1
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -11,12 +12,25 @@ let package = Package(
             targets: ["PropertyBased"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.1"),
+        .package(
+            url: "https://github.com/stackotter/swift-macro-toolkit.git",
+            revision: "ec8c5cf3b95cf9b0e1d4df6a0b2e0f524ae41558"),
+    ],
     targets: [
         .target(
             name: "PropertyBased",
-            dependencies: [],
-            exclude: ["PropertyCheck+Pack.swift.gyb", "Zip.swift.gyb"]
+            dependencies: [
+                "PropertyBasedMacros"
+            ]),
+        .macro(
+            name: "PropertyBasedMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+                .product(name: "MacroToolkit", package: "swift-macro-toolkit"),
+            ]
         ),
         .testTarget(
             name: "PropertyBasedTests",
--- Tests/PropertyBasedTests/IntegerAdditionTests.swift
+++ Tests/PropertyBasedTests/IntegerAdditionTests.swift
@@ -8,16 +8,20 @@
 import PropertyBased
 import Testing
 
+let randomInt = Gen.int(in: -100000...100000)
+
 /// A simple example of testing an operation by its properties.
 @Suite struct IntegerAdditionTests {
-
-    let randomInt = Gen.int(in: -100000...100000)
 
     @Test func testIdentity() async {
         await propertyCheck(input: randomInt) { a in
             #expect(a + 0 == a)
         }
     }
+
+    // @PropertyTest(input: randomInt) func testIdentity(a: Int) async {
+    //     #expect(a + 0 == a)
+    // }
 
     @Test func testCommutativity() async {
         await propertyCheck(input: randomInt, randomInt) { a, b in
