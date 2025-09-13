d1e0d668fe19ab8026c97deb94a4510fdfc27b0f
Lennard Sprong <lennard@buku.io>
2025-09-13 11:40:16 +0200

    Add "EnableShrinking" package trait for changing default behavior

--- Package.swift
+++ Package.swift
@@ -2,6 +2,12 @@
 
 import PackageDescription
 
+#if swift(>=6.2)
+let shrinkByDefault = true
+#else
+let shrinkByDefault = false
+#endif
+
 let package = Package(
     name: "PropertyBased",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13), .visionOS(.v1)],
@@ -10,6 +16,14 @@ let package = Package(
             name: "PropertyBased",
             targets: ["PropertyBased"]
         )
+    ],
+    traits: [
+        .default(enabledTraits: shrinkByDefault ? ["EnableShrinking"] : []),
+        .trait(
+            name: "EnableShrinking",
+            description:
+                "This trait enables the shrinker by default. Regardless of this trait being set, you can use the shrinking() TestTrait to enable/disable the shrinker on a case-by-case basis."
+        ),
     ],
     dependencies: [],
     targets: [
--- Sources/PropertyBased/EnableShrinkTrait.swift
+++ Sources/PropertyBased/EnableShrinkTrait.swift
@@ -11,7 +11,7 @@ import Testing
 ///
 /// Use ``shrinking(_:)`` to construct an instance of this trait.
 ///
-/// > Note: This feature is experimental, and disabled by default. The shrinking output will be very verbose, due to a limitation in Swift Testing.
+/// The Swift Package Trait `"EnableShrinking"` controls if shrinking defaults to being enabled. On Swift 6.2, this is set by default.
 public struct EnableShrinkTrait: TestTrait, SuiteTrait, TestScoping {
     @_documentation(visibility: internal)
     public var isRecursive: Bool { false }
@@ -24,16 +24,25 @@ public struct EnableShrinkTrait: TestTrait, SuiteTrait, TestScoping {
         }
     }
 
+    #if EnableShrinking
+    @TaskLocal static var isEnabled = true
+    #else
     @TaskLocal static var isEnabled = false
+    #endif
 
     var shouldEnable: Bool
 }
 
 extension Trait where Self == EnableShrinkTrait {
     /// Enable shrinking within this test or test suite.
+    ///
+    /// The Swift Package Trait `"EnableShrinking"` controls if shrinking defaults to being enabled. On Swift 6.2, this is set by default.
     public static var shrinking: Self { shrinking(true) }
 
     /// Enable or disable shrinking within this test or test suite.
+    ///
+    /// The Swift Package Trait `"EnableShrinking"` controls if shrinking defaults to being enabled. On Swift 6.2, this is set by default.
+    ///
     /// - Parameter enabled: Whether to enable shrinking.
     /// - Returns: An instance of ``EnableShrinkTrait``.
     public static func shrinking(_ enabled: Bool = true) -> Self {
